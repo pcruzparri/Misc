c9e5247975ce023a13e6b6da8efdb72e8dbbf1e5
pcruzparri <43578034+pcruzparri@users.noreply.github.com>
2023-06-30 13:54:38 -0500

    Polished overlap_beam.py and added normalization for gaussians.

--- beam_overlap.py
+++ beam_overlap.py
@@ -5,29 +5,34 @@ __all__ = ['interaction_volume']
 
 import numpy as np
 from scipy.ndimage import rotate
-import pyvista as pv
 import warnings
+from functools import reduce
+from collections.abc import Sequence
 
 @np.vectorize
-def makeGaussian(size, fwhm=3, center=None):
-    """Note: Copied without modification from StackOverflow user giessel.
-    
-    Make a square gaussian kernel.
+def makeGaussian(size, fwhm=3, center=None, amp=1, norm=False):
+    def gaussian():
+        """Note: Copied without modification from StackOverflow user giessel.
+        
+        Make a square gaussian kernel.
 
-    size is the length of a side of the square
-    fwhm is full-width-half-maximum, which
-    can be thought of as an effective radius.
-    """
-    x = np.arange(0, size, 1, float)
-    y = x[:, np.newaxis]
+        size is the length of a side of the square
+        fwhm is full-width-half-maximum, which
+        can be thought of as an effective radius.
+        """
+        x = np.arange(0, size, 1, float)
+        y = x[:, np.newaxis]
 
-    if center is None:
-        x0 = y0 = size // 2
-    else:
-        x0 = center[0]
-        y0 = center[1]
+        if center is None:
+            x0 = y0 = size // 2
+        else:
+            x0 = center[0]
+            y0 = center[1]
 
-    return np.exp(-4*np.log(2) * ((x-x0)**2 + (y-y0)**2) / fwhm**2)
+        return np.exp(-4*np.log(2) * ((x-x0)**2 + (y-y0)**2) / fwhm**2)
+    g = gaussian()
+    if norm: return amp*g/np.sum(g)
+    else: return g
 
 def clipped_arr(arr, thresh=0.5):
     return np.array(np.where(arr>thresh)).T
@@ -46,12 +51,12 @@ def get_plane_overlap(arr1, arr2, thresh=0.5, clip=(0,20), return_points=False):
     if return_points: return p1_arr, p2_arr #point locations in 3D matrix on two planes of the box along an axis
     else: return p1_arr.size #Due to symmetry, only get the size overlapping point list on one of the planes. 
 
-def interaction_volume(angles, fwhm, size, thresh=0.5, vis=False):
+def interaction_volume(angles, fwhm, size, thresh=0.5, vis=False, amp=[], norm=[]):
     '''Calculate and visualize multiple beam interaction
     
     Parameters:
     angles: (N, 3) array 
-        Contains the set of three rotation angles for N beams in degrees.
+        Contains the set of three rotation angles for N beams in degrees. Rotations are described in the [xy, yz, zx] planes.
 
     fwhm: (N, ) array
         Contains the FWHM for each beam in microns.
@@ -63,48 +68,65 @@ def interaction_volume(angles, fwhm, size, thresh=0.5, vis=False):
         Sets the minimum value used to visualize the gaussian-like beams and the interaction volume. Only used for visualization.
 
     vis: bool
-        Set whether or not to create pyvista point cloud plots.
+        Set whether or not to create pyvista point cloud plots. 
+    
+    amp: (N, ) float array
+        Set the intensity of each beam such that the sum of a slice through the axis of propagation is equal to the beam's intensity.
+
+    norm: (N, ) bool array
+        Set whether the beam intensity should be normalized and weighed by amp. amp does not apply without norm=True for the beam.
 
     Return:
     (vol_um3, vol_cm3)
         Returns a tuple of with the interaction volume of the beams in cubic microns and in cubic centimeters.
     '''
-
+    
+    print('Running preliminary tests...')
+    assert False not in list(map(lambda x: isinstance(x, Sequence) | isinstance(x, np.ndarray), 
+                                 [angles, fwhm, size, amp, norm])), 'angles, fwhm, size, amp, and norm must be iterables.'
     assert max(fwhm)<0.75*size[0] and max(fwhm)<0.75*size[1], 'Consider smaller FWHM relative to smallest box dimension for more accurate results.'
+    if amp or norm:
+        assert len(fwhm) == len(angles) == len(amp) == len(norm), 'The first axis of angles, fwhm, amp, and norm must have the same size.' 
+    else: 
+        assert len(fwhm) == len(angles), 'The first axis of angles and fwhm must have the same size.'
+        amp = np.ones(len(fwhm))
+        norm = np.array([False]*len(fwhm))
 
     # Preparing Matrices
     print('Preparing matrices...')
     ls_beams = []
-    assert len(fwhm) == len(angles), 'The first axis of angles and fwhm must have the same size.'
     for beam in range(len(fwhm)):
         print(beam)
-        arr2d = makeGaussian(size[0], fwhm=fwhm[beam])
+        arr2d = makeGaussian(size[0], fwhm=fwhm[beam], amp=amp[beam], norm=norm[beam])
         arr3d = np.array([arr2d]*size[1])
         for rot_ind, rot in enumerate(angles[beam]):
             arr3d = rotate(arr3d, rot, axes=(rot_ind, (rot_ind+1)%3), reshape=False, mode='nearest')
         ls_beams.append(arr3d)
 
     print('Checking inclusion of interaction volume in box')
-    for beam in range(3):
+    for beam in range(len(angles)):
         if get_plane_overlap(*(ls_beams[beam],ls_beams[(beam+1)%3]), thresh=thresh, clip=(0,size[1]-1)):
             warnings.warn(f'Beams {beam} and {(beam+1)%3} are overlapping at the edges. Consider extending the box size or changing the angles.')
 
     vis = True
+    assert thresh<np.min([np.max(i) for i in ls_beams]), f'Thresh should be less than {np.min(np.max(ls_beams, axis=1))}'
     if vis:
+        from pyvista import Plotter, PolyData, global_theme
+        global_theme.color_cycler = 'default'
+
         print('Visualizing...')
         # Plotting beams
-        plotter = pv.Plotter()
-        plotter.add_mesh(clipped_arr(ls_beams[0], thresh=thresh), color='g', point_size=10, opacity=0.7)
-        plotter.add_mesh(clipped_arr(ls_beams[1], thresh=thresh), color='r', point_size=10, opacity=0.7)
-        plotter.add_mesh(clipped_arr(ls_beams[2], thresh=thresh), color='b', point_size=10, opacity=0.7)
+        plotter = Plotter()
+        for beam in ls_beams:
+            plotter.add_mesh(clipped_arr(beam, thresh=thresh), point_size=10, opacity=0.7)
         plotter.show_grid()
         plotter.show()
         #Plotting interaction volume
-        int_plotter = pv.PolyData(clipped_arr(np.multiply(np.multiply(ls_beams[0], ls_beams[1]), ls_beams[2]), thresh=thresh))
+        int_plotter = PolyData(clipped_arr(reduce(np.multiply, ls_beams), thresh=thresh**len(fwhm)))
         int_plotter.plot()
 
     # Calculating volume
     print('Calculating volume...')
-    vol_um3 = np.sum(np.multiply(np.multiply(ls_beams[0], ls_beams[1]), ls_beams[2])) # um^3
+    vol_um3 = np.sum(reduce(np.multiply, ls_beams)) # um^3
     vol_cm3 = vol_um3*(10**-12)
     return vol_um3, vol_cm3
